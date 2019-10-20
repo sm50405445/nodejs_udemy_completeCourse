e5cf81f9964264d186e1bf14fb468c77c624cf66
sm50405445 <46436097+sm50405445@users.noreply.github.com>
2019-10-20 23:36:49 +0900

    1020 yargs 사용법 및 메이킹 builder

--- note-app/app.js
+++ note-app/app.js
@@ -1,15 +1,66 @@
-const validator = require('validator');
+const yargs = require('yargs');
 const chalk = require('chalk');
 const getNotes = require('./note.js');
 
-console.log(getNotes());
-console.log(validator.isEmail("sd50712naver.com"));
-const msg = chalk.green.bold.inverse('Success!');
-console.log(msg);
+// 10-19 version
+// console.log(getNotes());
+// console.log(validator.isEmail("sd50712naver.com"));
+// const msg = chalk.green.bold.inverse('Success!');
+// console.log(msg);
 
-console.log(process.argv[2]);
+//Customizing yargs
+yargs.version('1.1.0');
+
+//yargs create command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title : ' + argv.title);
+        console.log("body : " + argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function () {
+        console.log('remove a note');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('read a note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list notes',
+    handler: function () {
+        console.log('list notes');
+    }
+})
 
 
-// const add = require('./utils.js');
-// const value = add(4, -2);
-// console.log(value);
+//add, remove, read, list
+
+yargs.parse();
+// console.log(yargs.argv);
+
+
