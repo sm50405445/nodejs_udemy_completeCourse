532a9d41977f1397aecd3536a83a866d86792113
sm50405445 <46436097+sm50405445@users.noreply.github.com>
2019-10-30 23:36:58 +0900

    20191030 weatherApp seperate

---
+++ playground/4-callback.js
@@ -0,0 +1,43 @@
+setTimeout(() => {
+    console.log("2second setTimeOut")
+}, 2000)
+
+const names = ["Andrew", "Leesang", "cho"];
+const shortNames = names.filter((name) => {
+    return name.length <= 4;
+})
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            longitude: 0,
+            latitude: 0
+        }
+
+        callback(data);
+    }, 2000);
+}
+
+geocode("Philadelphia", (data) => {
+    console.log(data);
+});
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        const data = a + b;
+
+        callback(data);
+    }, 2000)
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
--- weather-app/app.js
+++ weather-app/app.js
@@ -9,8 +9,6 @@ const darkSkyurl = 'https://api.darksky.net/forecast/9b207953cac30c883ec5890496f
 //     console.log("It is currently " + curTemperature + " degrees out. There is a " + precipProbability + "% chance of rain");
 // });
 
-const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2Q1MDcxMjMyMSIsImEiOiJjazI3Z3RzenAwNnB5M2xtYm8wc2s4bXA1In0.ZezIvmBEcMLZHMUQIx74gg';
-
 
 // request({url:mapboxUrl,json:true},(error,response)=>{
 //     const jsonData = response.body;
@@ -23,24 +21,45 @@ const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angel
 
 // })
 
-request({url:darkSkyurl,json:true},(error,response)=>{
-    if(error){
-        console.log('error')
-    }else if(error.errno === "ENOTFOUND"){
-        console.log('network err')
-    }else if(response.body.features.length ===0){
-        console.log('Unable to connect to location services!')
-    }
-    else{
-        const jsonData = response.body;
+// request({url:darkSkyurl,json:true},(error,response)=>{
+//     if(error){
+//         console.log('error')
+//     }else if(error.errno === "ENOTFOUND"){
+//         console.log('network err')
+//     }else if(response.body.features.length ===0){
+//         console.log('Unable to connect to location services!')
+//     }
+//     else{
+//         const jsonData = response.body;
 
-        const latitude = jsonData.features[0].geometry.coordinates[1];
-        const longitude = jsonData.features[0].geometry.coordinates[0];
+//         const latitude = jsonData.features[0].geometry.coordinates[1];
+//         const longitude = jsonData.features[0].geometry.coordinates[0];
 
-        console.log('longitude = '+longitude);
-        console.log('latitude = '+latitude);
-    }
+//         console.log('longitude = '+longitude);
+//         console.log('latitude = '+latitude);
+//     }      
+// })
 
-        
+const geocode = (address, callback) => {
+    const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2Q1MDcxMjMyMSIsImEiOiJjazI3Z3RzenAwNnB5M2xtYm8wc2s4bXA1In0.ZezIvmBEcMLZHMUQIx74gg';
+
+    request({ url: mapboxUrl, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services', undefined);
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try another search', undefined);
+        } else {
+            callback(undefined, {
+                longitude: response.body.features[0].center[0],
+                latitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+geocode('Philadelphia', (error, data) => {
+    console.log("Error", error);
+    console.log("data", data);
 })
 
