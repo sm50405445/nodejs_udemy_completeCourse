3a4c29f533e79897851a4631af081161cad11a2e
sm50405445 <46436097+sm50405445@users.noreply.github.com>
2019-11-14 23:23:06 +0900

    2019-11-14
    view Engine and node setting

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,51 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-app.get('', (req, res) => {
-    res.send("Hello Express")
-})
+//Define paths for Express config
+const publicFileDir = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-app.get('/help', (req, res) => {
-    res.send('Help Page')
+//Setup handlers engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory
+app.use(express.static(publicFileDir));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'LSM'
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.render('about', {
+        title: 'about',
+        img: '/img/_img34.png',
+        name: 'LSM'
+    })
 })
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        name: 'LSM'
+    })
+})
+
+
+
 app.get('/weather', (req, res) => {
-    res.send('Weather Page')
+    res.send('Weather Page', {
+        title: 'weather',
+        name: 'LSM'
+    })
 })
 
 app.listen(3000, () => {
