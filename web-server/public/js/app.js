92a572bda4c5a47778f345e944028b8bcc19bfa3
sd50712321 <sd50712321@naver.com>
2019-12-15 23:05:05 +0900

    20191215

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -19,15 +19,15 @@ weatherForm.addEventListener('submit',(e)=>{
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent = data.error
         }else{
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecast
-            console.log(data.location)
             console.log(data.forecast)
+            console.log(data.location)
         }
     })
 })
