aa1cb5437302ec05d575aae7475d037c8905aa3c
sd50712321 <sd50712321@naver.com>
2020-01-21 00:13:24 +0900

    update

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip,fahrenheitToCelsius,celsiusToFahrenheit} = require('../src/math')
+const {calculateTip,fahrenheitToCelsius,celsiusToFahrenheit,add} = require('../src/math')
 
 test('Should calculate total with tip',()=>{
     const total = calculateTip(10, .3)
@@ -22,4 +22,17 @@ test('Should convert 32 F to 0 C',()=>{
 test('Should convert 0 C to 32 F',()=>{
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+test('Should add two numbers',(done)=>{
+    add(2,3).then((sum)=>{
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add two numbers async/await',async()=>{
+    const sum = await add(20,3)
+    expect(sum).toBe(23)
+
 })
