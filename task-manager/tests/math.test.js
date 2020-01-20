5160bacc10b1dbba13f18c1aa664d16164a8949d
sd50712321 <sd50712321@naver.com>
2020-01-20 23:13:59 +0900

    update 20200120

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,25 @@
-test('Hello World',()=>{
+const {calculateTip,fahrenheitToCelsius,celsiusToFahrenheit} = require('../src/math')
 
+test('Should calculate total with tip',()=>{
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
+
+    // if(total !== 13){
+    //     throw new Error('Total tip should be 13. Got '+total)
+    // }
 })
 
-test('This should fail',(err)=>{
-    throw new Error(err)
+test('Should caculate total with default tip',()=>{
+    const total = calculateTip(10)
+    expect(total).toBe(12)
+})
+
+test('Should convert 32 F to 0 C',()=>{
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('Should convert 0 C to 32 F',()=>{
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
 })
